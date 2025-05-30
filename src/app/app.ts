0953d7160510bbeb140c81a6727bf46f075a273f
Nitin Sutar <nsutar1990@gmail.com>
2025-05-31 00:16:42 +0530

    Update TailwindCSS configuration

---
+++ src/app/app.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.html',
+  styleUrl: './app.scss'
+})
+export class App {
+  protected title = 'Tailwind-POC';
+}
